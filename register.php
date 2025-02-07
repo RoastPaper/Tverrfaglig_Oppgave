416f8c145b5f3e0929343c4da0a5959f9b7896fc
RoastPaper <william.haaje@gmail.com>
2025-02-07 13:47:02 +0000

    Jeg har nå endret litt på index.html. Jeg er ferdig med oppbygning av registeringen til nettsiden. Må sjekke om det fungerer.

--- register.php
+++ register.php
@@ -14,6 +14,34 @@ if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
 if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
     exit('Please complete the registration form');
 }
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username= ?')) {
-    
+if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    exit('Email is not valid!');
 }
+if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
+    exit('Username is not valid!');
+}
+if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
+    exit('Password must be between 5 and 20 characters long!');
+}
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username= ?')) {
+    $stmt->bind_param('s', $_POST['username']);
+    $stmt->execute();
+    $stmt->store_result();
+    if ($stmt->num_rows > 0) {
+        echo 'Username exists, please choose another!';
+    } else {
+        if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
+            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
+            $stmt->execute();
+            echo 'You have successfully registered, you can now login!';
+        } else {
+            echo 'Could not prepare statement!';
+        }
+    }
+    $stmt->close();
+} else {
+    echo 'Could not prepare statement!';
+}
+$con->close();
+?>
