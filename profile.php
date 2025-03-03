5438408a1689c7b9745ca72200a2a8c5f414cc50
RoastPaper <william.haaje@gmail.com>
2025-03-03 10:56:21 +0000

    Fjernet informason om quiz grunn av det ikke ferdig som gjør at siden fungere ikke. Kommentarer for å hjelpe meg og andre brukerer.

--- profile.php
+++ profile.php
@@ -1,22 +1,27 @@
 <?php
 session_start();
 
+// Sjekker om brukeren er logget inn hvis brukeren er ikke sender til hjemsiden som er logget ut.
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
 }
+// Dette gjeler her også som jeg forkalrte i login.php.
 require_once('/var/www/config.php');
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-$stmt = $con->prepare('SELECT password, email, date FROM accounts WHERE id = ?');
-$stmt = $con->prepare('SELECT title FROM quiz WHERE id = ?');
+// SQL-spørringer for password, email, date fra accounts ved å bruke id til kontoen i table.
+$stmt = $con->prepare('SELECT password, email, username, date FROM accounts WHERE id = ?');
+// SQL-spørringer for title fra quiz.
+// $stmt = $con->prepare('SELECT title FROM quiz WHERE id = ?'); Denne koden fungerer ikke grunn av jeg ble ikke ferdig med koden i quiz.php. 
 
+// HEr finner det frem informasjon fra table fra toppen. Binder det sammen og finner det frem for at dek viser frem i HTML documentet.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
-$stmt->bind_result($password, $email, $birthdate, $title);
+$stmt->bind_result($password, $email, $birthdate, $username); // $title skal også være i det, men fungerer grunn av ble ikke ferdig med quiz.php.
 $stmt->fetch();
 $stmt->close();
 ?>
@@ -42,10 +47,12 @@ $stmt->close();
     <h2>Profile Page</h2>
     <div>
       <p>Your account details are below:</p>
+
+      <!-- Her bruker de som jeg bindet i php. Dette viser frem i nettsiden. -->
       <table>
         <tr>
           <td>Username:</td>
-          <td><?=htmlspecialchars($_SESSION['name'], ENT_QUOTES)?></td>
+          <td><?=htmlspecialchars($username, ENT_QUOTES)?></td>
         </tr>
         <tr>
           <td>Password:</td>
