5cb0d3bdeeaf81729a7a18d09a384ccbad77e03d
RoastPaper <william.haaje@gmail.com>
2025-03-03 11:10:12 +0000

    Kommentarer.

--- register.php
+++ register.php
@@ -12,34 +12,43 @@ $day   = intval($_POST['day']);
 $month = intval($_POST['month']);
 $year  = intval($_POST['year']);
 
+// Kobler mysql til php
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
+if (mysqli_connect_errno()) { // Sjekker om noe feil med koblingen mellom mysql og php.
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
+// Sjekker om brukeren har skrevt inn alle feltene rikitg.
 if (!isset($username, $password, $email, $day, $month, $year)) {
     exit('Please complete the registration form!');
 }
+// Sjekker om det er tomt.
 if (empty($username) || empty($password) || empty($email) || empty($day) || empty($month) || empty($year)) {
     exit('Please complete the registration form');
 }
 
+// Sjekker om emailen brukeren tastet er faktisk en email.
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     exit('Email is not valid!');
 }
 
+// Koden funksjonen preg_match for å sjekker om $username kun inneholder bokstaver som er små og store. Koden bruker en regulært uttrykk, som betyr fra start til slutt må det inneholder minst en alfanumerisk karakter.
 if (preg_match('/^[a-zA-Z0-9]+$/', $username) == 0) {
     exit('Username is not valid!');
 }
 
+// Koden bruker funksjonen strlen for å sjekke hvor langt passordet er. Sjekker om det er 5 tegn eller lengre enn 20 tegn.
 if (strlen($password) > 20 || strlen($password) < 5) {
     exit('Password must be between 5 and 20 characters long!');
 }
 
+// Sjekker om brukeren har put inn riktig informasjon i datoen ved å bruke funksjonen checkdate.
 if (!checkdate($month, $day, $year)) {
     exit('Invalid birthdate provided!');
 }
-$birthdate = sprintf("%04d-%02d-%02d", $year, $month, $day);
+
+// Her bruker koden funksjonen sprintf for å lage en formatet streng som representerer en fødseldato.
+$birthdate = sprintf("%04d-%02d-%02d", $year, $month, $day); // "%04d" betyr år, "%02" er måned og dag. 
 
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
     $stmt->bind_param('s', $username);
